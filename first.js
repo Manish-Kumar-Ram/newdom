99f3e5ba219bf4e3c4716676f4c73873070680ee
Manish Kumar Ram <manishkaran9809@gmail.com>
2024-02-28 23:04:21 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,15 @@
+const myh1=document.getElementById("myh1");
+console.log(myh1)
+myh1.style.color='blue';
+myh1.style.backgroundColor='yellow';
+myh1.style.width='300px'
+myh1.style.height='200px'
+myh1.style.margin='20px'
+myh1.style.padding='10px'
+
+myh1.style.fontWeight='bold'
+
+myh1.style.fontSize='18px'
+
+
+
